f7e94d5c8ea7109a0c294046d6aeea3a33fd2360
rizkarahayyu <rizkarahayu42@gmail.com>
2019-05-06 13:01:38 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { CollectionComponent } from './collection/collection.component';
+import { ItemListComponent } from './collection/item-list/item-list.component';
+import { ItemDetailComponent } from './collection/item-detail/item-detail.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    CollectionComponent,
+    ItemListComponent,
+    ItemDetailComponent,
+    ContactUsComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule.forRoot(),
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
